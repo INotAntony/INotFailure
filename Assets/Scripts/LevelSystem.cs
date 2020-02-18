8c5af07333c9bb9d189567005eb9a2fdf8bbf944
INotAntony <antonio.garcia@student.saedubai.com>
2020-02-18 19:15:42 +0400

    Commitment

---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    public int XPcount;
+    public int currentlevel;
+    // Start is called before the first frame update
+    void Update()
+    {
+        UpdateXp (5);
+    }
+
+    // Update is called once per frame
+    public void UpdateXp(int xp)
+    {
+        XP + -xp;
+        int ourlevel=(int)(0.1f*Mathf.Sqrt(xp));
+        if (ourlevel = currentlevel)
+        {
+            currentlevel = ourlevel;
+        }
+        int xpnextlevel = 100 * (currentlevel + 1) * (currentlevel + 1);
+        int xpdifference = xpnextlevel - xp;
+        int totalxpdifference = xpnextlevel - (100 * currenlevet * currentlevel);
+    }
+}
